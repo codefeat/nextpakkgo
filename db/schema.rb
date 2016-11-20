847936bac7850770a4250dfd967e89e109aeab03
Lowell Fortune <lowellfortune@gmail.com>
2016-11-20 11:34:09 -0600

    Linked reviews to users

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161120163329) do
+ActiveRecord::Schema.define(version: 20161120172303) do
 
   create_table "deliveries", force: :cascade do |t|
     t.string   "name"
@@ -28,6 +28,7 @@ ActiveRecord::Schema.define(version: 20161120163329) do
     t.text     "comment"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer  "user_id"
   end
 
   create_table "users", force: :cascade do |t|
